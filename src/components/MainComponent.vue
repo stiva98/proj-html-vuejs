1d0975ec6fc3fa1fdfec13ffb928bbc2d0de78dc
stiva98 <samustiva9810@gmail.com>
2023-07-25 17:10:39 +0200

    terminata sezione 3 con parte dinamica e layout

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,7 +3,62 @@ export default {
     name: "MainComponent",
     data() {
         return {
-
+            cardsCourses: [
+                {
+                    cardImage: "../src/assets/img/electronic.jpeg",
+                    cardTitle: "How to be a DJ? Make Electronic Music",
+                    cardParagraph: "Electronic",
+                    cardDifficult: "Advanced",
+                    cardDuration: "8 lectures",
+                    cardHours: "6 hours",
+                    cardButton: true  
+                },
+                {
+                    cardImage: "../src/assets/img/nvidia.png",
+                    cardTitle: "Nvidia and UE4 Technologies Practice",
+                    cardParagraph: "Nvidia",
+                    cardDifficult: "Advanced",
+                    cardDuration: "8 lectures",
+                    cardHours: "6 hours",
+                    cardButton: true
+                },
+                {
+                    cardImage: "../src/assets/img/fashion.jpeg",
+                    cardTitle: "Fashion Photography from professional",
+                    cardParagraph: "Fashion",
+                    cardDifficult: "Advanced",
+                    cardDuration: "6 lectures",
+                    cardHours: "6 hours",
+                    cardButton: true
+                },
+                {
+                    cardImage: "../src/assets/img/communication.jpeg",
+                    cardTitle: "Design Instruments for Communication",
+                    cardParagraph: "Communication",
+                    cardDifficult: "Intermediate",
+                    cardDuration: "6 lectures",
+                    cardHours: "6 hours",
+                    cardButton: false
+                },
+                {
+                    cardImage: "../src/assets/img/art.jpg",
+                    cardTitle: "Make your Concept Right and Beatiful",
+                    cardParagraph: "Art",
+                    cardDifficult: "Intermediate",
+                    cardDuration: "6 lectures",
+                    cardHours: "6 hours",
+                    cardButton: false
+                },
+                {
+                    cardImage: "../src/assets/img/bicycling.jpeg",
+                    cardTitle: "Road Bike Manual or How to Be a Champion",
+                    cardParagraph: "Electronic",
+                    cardDifficult: "Bicycling",
+                    cardDuration: "6 lectures",
+                    cardHours: "6 hours",
+                    cardButton: false
+                },
+            ]
         }
     },
     methods: {
@@ -30,7 +85,7 @@ export default {
                             <span class="mini-text-h4">Limited sale:</span> All courses with 55% off
                         </h4>
                         <div class="input-group mb-3 bg-white rounded-pill">
-                            <input type="text" class="form-control bg-transparent border border-0" placeholder="Enter your email"  aria-describedby="button-addon2">
+                            <input type="text" class="form-control bg-transparent border border-0 shadow" placeholder="Enter your email"  aria-describedby="button-addon2">
                             <button class="btn rounded-pill" type="button" id="button-addon2">SUBSCRIPE</button>
                         </div>
                     </div>
@@ -90,6 +145,61 @@ export default {
                 </div>
             </div>
         </section>
+        <section class="third-section">
+            <div class="my-container">
+                <h2 class="text-center fw-bold pt-5 mb-5">
+                    New Courses
+                </h2>
+                <div class="row">
+                    <div class="col-4 pb-5" v-for="(card, cardIndex) in cardsCourses" :key="cardIndex">
+                        <div class="card" style="width: 18rem;">
+                            <div class="container-image position-relative">
+                                <img :src="card.cardImage" class="card-img-top" :alt="card.cardParagraph">
+                                <button class="btn btn-primary position-absolute" v-if="card.cardButton">
+                                    SPECIAL
+                                </button>
+                                <div class="price position-absolute h-100 w-100">
+                                    $59
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">
+                                    {{ card.cardTitle }}
+                                </h5>
+                                <p class="card-text">
+                                    {{ card.cardParagraph }}
+                                </p>
+                                <div class="row">
+                                    <div class="col-4 pe-0">
+                                        <i class="fa-solid fa-signal"></i>
+                                        <span>
+                                            {{ card.cardDifficult}}
+                                        </span>
+                                    </div>
+                                    <div class="col-4 pe-0">
+                                        <i class="fa-solid fa-list"></i>
+                                        <span>
+                                            {{ card.cardDuration}}
+                                        </span>
+                                    </div>
+                                    <div class="col-4">
+                                        <i class="fa-regular fa-clock"></i>
+                                        <span>
+                                            {{ card.cardHours}}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center pt-4 pb-5 container-button">
+                    <button class="text-white rounded-pill">
+                        LOAD MORE
+                    </button>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -110,6 +220,9 @@ main {
             background-color: $divAndFooterBg;
             max-width: 1052px;
             left: 15%;
+        }
+        .shadow {
+            box-shadow: none;
         }
         button {
             background-color: $button;
@@ -133,7 +246,7 @@ main {
         }
     }
     .second-section {
-        margin-top: 100px;
+        margin: 100px 0;
         .my-container {
             max-width: 992px;
             margin: 0 auto;
@@ -144,6 +257,63 @@ main {
         .col-6 > p {
             font-size: small;
             line-height: 25px;
+        }
+    }
+    .third-section {
+        background-color: $sectionMainBg;
+        .my-container {
+            max-width: 992px;
+            margin: 0 auto;
+        }
+        .container-button > button {
+            margin: 30px 0 20px 0;
+            background-color: $divAndFooterBg;
+            padding: 10px;
+            border: none;
+            font-size: small;
+        }
+        .col-4 h5, .col-4 p{
+            text-align: center;
+        }
+        .col-4 p {
+            color: $colorTextCard;
+            font-size: small;
+            padding-bottom: 30px;
+            border-bottom: 1px solid $colorTextCard;
+        }
+        .container-image > button {
+            top: 0;
+            right: 0;
+            margin: 10px;
+            font-size: xx-small;
+            padding: 5px;
+            background-color: $divAndFooterBg;
+            border: none;
+            z-index: 999;
+        }
+        .col-4 span {
+            font-size: x-small;
+            vertical-align: middle;
+            padding-left: 2px;
+        }
+        .col-4 i {
+            color: $divAndFooterBg;
+        }
+        .price {
+            top: 0;
+            left: 0;
+            color: white;
+            font-size: x-large;
+            background-color: rgba($color: #000000, $alpha: 0.5);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            visibility: hidden;
+            opacity: 0;
+        }
+        .container-image:hover .price{
+            visibility: visible;
+            opacity: 1;
         }
     }
 }
