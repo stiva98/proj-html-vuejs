05ce3c1833d4561cae3c4a18e83291cb8a289cb4
stiva98 <samustiva9810@gmail.com>
2023-07-26 12:38:20 +0200

    footer terminato

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -46,11 +46,77 @@ export default {
                     <h5>
                         PAGES
                     </h5>
+                    <div class="d-flex">
+                        <div class="col-6">
+                            <ul>
+                                <li>
+                                    <a href="">
+                                        Blog
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Shortcodes
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <ul>
+                                <li>
+                                    <a href="">
+                                        Courses
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Membership A...
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Typography
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-3">
                     <h5>
                         BLOG
                     </h5>
+                    <div class="d-flex">
+                        <div class="col-4">
+                            <img src="../assets/img/photo-1-footer.jpeg" alt="foto-1">
+                        </div>
+                        <div class="col-8">
+                            <h6 class="fw-bold">
+                                Our main target is to "Developing Yourself as a Leader"
+                            </h6>
+                            <p class="text-last-column">
+                                - August 9, 2018
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="col-4">
+                            <img src="../assets/img/photo-2-footer.jpeg" alt="foto-2">
+                        </div>
+                        <div class="col-8">
+                            <h6 class="fw-bold">
+                                Those Other Collage Expense You Aren't Thinking About
+                            </h6>
+                            <p class="text-last-column">
+                                - June 3, 2015
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -86,5 +152,43 @@ footer {
         color: white;
         cursor: pointer;
     }
+    ul {
+        padding-left: 15px;
+
+        li{
+            margin-bottom: 15px;
+            font-size: small;
+        }
+        a{
+            color: $colorTextFooter;
+            text-decoration: none;
+        }
+        li:hover{
+            color: white;
+        }
+        li:hover a{
+            color: white;
+        }
+    }
+    .d-flex {
+        margin-bottom: 15px;
+        .col-8 {
+            margin-left: 15px;
+        }
+        img {
+            width: 100%;
+        }
+        h6 {
+            font-size: small;
+        }
+        .text-last-column {
+            color: $colorTextCard;
+        }
+        .text-last-column:hover {
+            color: $colorTextCard;
+            cursor: default;
+        }
+    }
+
 }
 </style>
